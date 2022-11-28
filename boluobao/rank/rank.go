40079f83bee34a2497e991bf6f3a72a4c3992b8a
VeronicaAlexia <Elaina-Alex@proton.me>
2022-11-28 21:05:52 +0800

    ~

--- boluobao/rank/rank.go
+++ boluobao/rank/rank.go
@@ -5,46 +5,25 @@ import (
 	"github.com/VeronicaAlexia/BoluobaoAPI/boluobao/request"
 )
 
-type Rank struct{}
+type Rank struct {
+	RtypeIndex int //view, sale, newhit, mark, ticket, bonus
+	Month      bool
+	All        bool
+}
 
-func (r *Rank) GET_POPULARITY_RANKS() BoluobaoStructs.Rank {
+func (r *Rank) rank_api() string {
+	if r.Month {
+		return "ranks/month/novels"
+	} else if r.All {
+		return "ranks/all/novels"
+	} else {
+		return "ranks/week/novels"
+	}
+}
+func (r *Rank) GET_SFACG_RANKS() BoluobaoStructs.Rank {
 	var RankStruct BoluobaoStructs.Rank
 	params := map[string]string{"page": "0", "size": "50", "rtype": "view", "ntype": "origin", "expand": "typeName,tags,ticket,latestchapter"}
-	request.Get("ranks/week/novels").AddAll(params).NewRequests().Unmarshal(&RankStruct)
-	return RankStruct
-}
-
-func (r *Rank) GET_BEST_SELLER_RANKS() BoluobaoStructs.Rank {
-	var RankStruct BoluobaoStructs.Rank
-	params := map[string]string{"page": "0", "size": "50", "rtype": "sale", "ntype": "origin", "expand": "typeName,tags,ticket,latestchapter"}
-	request.Get("ranks/week/novels").AddAll(params).NewRequests().Unmarshal(&RankStruct)
-	return RankStruct
-}
-
-func (r *Rank) GET_NEW_NOVEL_RANKS() BoluobaoStructs.Rank {
-	var RankStruct BoluobaoStructs.Rank
-	params := map[string]string{"page": "0", "size": "50", "rtype": "newhit", "ntype": "origin", "expand": "typeName,tags,ticket,latestchapter"}
-	request.Get("ranks/week/novels").AddAll(params).NewRequests().Unmarshal(&RankStruct)
-	return RankStruct
-}
-
-func (r *Rank) GET_MARK_RANKS() BoluobaoStructs.Rank {
-	var RankStruct BoluobaoStructs.Rank
-	params := map[string]string{"page": "0", "size": "50", "rtype": "mark", "ntype": "origin", "expand": "typeName,tags,ticket,latestchapter"}
-	request.Get("ranks/week/novels").AddAll(params).NewRequests().Unmarshal(&RankStruct)
-	return RankStruct
-}
-
-func (r *Rank) GET_TICKET_RANKS() BoluobaoStructs.Rank {
-	var RankStruct BoluobaoStructs.Rank
-	params := map[string]string{"page": "0", "size": "50", "rtype": "ticket", "ntype": "origin", "expand": "typeName,tags,ticket,latestchapter"}
-	request.Get("ranks/week/novels").AddAll(params).NewRequests().Unmarshal(&RankStruct)
-	return RankStruct
-}
-
-func (r *Rank) GET_BONUS_RANKS() BoluobaoStructs.Rank {
-	var RankStruct BoluobaoStructs.Rank
-	params := map[string]string{"page": "0", "size": "50", "rtype": "bonus", "ntype": "origin", "expand": "typeName,tags,ticket,latestchapter"}
-	request.Get("ranks/week/novels").AddAll(params).NewRequests().Unmarshal(&RankStruct)
+	params["rtype"] = []string{"view", "sale", "newhit", "mark", "ticket", "bonus"}[r.RtypeIndex]
+	request.Get(r.rank_api()).AddAll(params).NewRequests().Unmarshal(&RankStruct)
 	return RankStruct
 }
