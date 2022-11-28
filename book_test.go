87e8b1ca916099d24237bfb250fe86ce0eb7380f
VeronicaAlexia <Elaina-Alex@proton.me>
2022-11-28 21:46:09 +0800

    ~

--- book_test.go
+++ book_test.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"fmt"
-	"github.com/VeronicaAlexia/BoluobaoAPI/boluobao"
+	"github.com/VeronicaAlexia/BoluobaoAPI/boluobao/rank"
+	"github.com/VeronicaAlexia/BoluobaoAPI/request"
+
 	"testing"
 )
 
 func TestBook(t *testing.T) {
-	fmt.Println(boluobao.GET_BOOK_INFORMATION("551946"))
+	App := request.AppRequest{App: false}
+	App.SetApiHost()
+	ranks := rank.Rank{RtypeIndex: 0, Page: 0}
+	for index, i2 := range ranks.GET_SFACG_RANKS().Data {
+		fmt.Println(index, ": ", i2.NovelName)
+	}
 }
-func TestCATALOGUE(t *testing.T) {
-	fmt.Println(boluobao.GET_CATALOGUE("551946"))
-}
+
+//func TestCATALOGUE(t *testing.T) {
+//	fmt.Println(boluobao.GET_CATALOGUE("551946"))
+//}
