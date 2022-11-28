df509b5014a7d7c1e4d15b0eb0eccdfabae4f8cb
VeronicaAlexia <Elaina-Alex@proton.me>
2022-11-28 21:15:01 +0800

    ~

--- boluobao/rank/rank.go
+++ boluobao/rank/rank.go
@@ -1,14 +1,17 @@
 package rank
 
 import (
+	"fmt"
 	"github.com/VeronicaAlexia/BoluobaoAPI/BoluobaoStructs"
 	"github.com/VeronicaAlexia/BoluobaoAPI/boluobao/request"
+	"strconv"
 )
 
 type Rank struct {
 	RtypeIndex int //view, sale, newhit, mark, ticket, bonus
 	Month      bool
 	All        bool
+	Page       int
 }
 
 func (r *Rank) rank_api() string {
@@ -22,8 +25,13 @@ func (r *Rank) rank_api() string {
 }
 func (r *Rank) GET_SFACG_RANKS() BoluobaoStructs.Rank {
 	var RankStruct BoluobaoStructs.Rank
-	params := map[string]string{"page": "0", "size": "50", "rtype": "view", "ntype": "origin", "expand": "typeName,tags,ticket,latestchapter"}
+	params := map[string]string{"size": "50", "rtype": "view", "ntype": "origin", "expand": "typeName,tags,sysTags,ticket,latestchapter"}
+	if r.RtypeIndex >= 6 {
+		fmt.Println("RtypeIndex must be less than 6")
+		r.RtypeIndex = 0
+	}
+	params["page"] = strconv.Itoa(r.Page)
 	params["rtype"] = []string{"view", "sale", "newhit", "mark", "ticket", "bonus"}[r.RtypeIndex]
-	request.Get(r.rank_api()).AddAll(params).NewRequests().Unmarshal(&RankStruct)
+	request.Get(r.rank_api()).AddAll(params).NewRequests().Unmarshal(&RankStruct).WriteResultString()
 	return RankStruct
 }
