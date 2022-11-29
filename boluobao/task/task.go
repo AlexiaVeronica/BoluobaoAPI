17fc3b4fe3fb77d6c5fcaffb9c092af402a07551
VeronicaAlexia <Elaina-Alex@proton.me>
2022-11-29 18:57:34 +0800

    ~

--- boluobao/task/task.go
+++ boluobao/task/task.go
@@ -17,8 +17,7 @@ func (task *Task) GET_SIGN_INFO() {
 	var SignIn Template.SignIn
 	// "user/signInfo" is old api, now it's "user/newSignInfo"
 	today := time.Now().Format("2006-01-02")
-	request.Put("user/newSignInfo").AddAll(map[string]string{"signDate": today}).NewRequests().Unmarshal(&SignIn)
-	//fmt.Println(SignIn)
+	request.Put("user/newSignInfo").Add("signDate", today).NewRequests().Unmarshal(&SignIn)
 	if SignIn.Status.HTTPCode == 200 {
 		fmt.Println("sign in success, date: ", today)
 	} else {
@@ -40,6 +39,7 @@ func (task *Task) GET_TASKS_LIST() {
 }
 
 func (task *Task) RECEIVE_TASK() {
+	task.GET_TASKS_LIST() // init task list
 	for _, data := range task.TaskList.Data {
 		fmt.Println("任务名称:", data.Name, "尝试领取")
 		request.Post("user/tasks/" + strconv.Itoa(data.TaskId)).AddAll(map[string]string{
@@ -49,17 +49,6 @@ func (task *Task) RECEIVE_TASK() {
 		}).NewRequests()
 	}
 }
-func (task *Task) POST_TASK_LIST() {
-	ListenData := map[string]string{
-		"seconds":     "3605",
-		"readingDate": time.Now().Format("2006-01-02"),
-		"entityType":  "3",
-	}
-	request.Post("user/tasks/4").AddAll(ListenData).NewRequests().WriteResultString()
-	request.Post("user/tasks/5").AddAll(ListenData).NewRequests().WriteResultString()
-	request.Post("user/tasks/17").AddAll(ListenData).NewRequests().WriteResultString()
-}
-
 func (task *Task) COMPLETE_TASK_LIST() {
 	ListenData := map[string]string{
 		"seconds":     "3605",
