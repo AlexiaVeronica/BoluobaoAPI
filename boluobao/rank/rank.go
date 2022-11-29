48c7359f62eb055421da8ed2a1fcd54641771f52
VeronicaAlexia <Elaina-Alex@proton.me>
2022-11-29 16:39:52 +0800

    ~

--- boluobao/rank/rank.go
+++ boluobao/rank/rank.go
@@ -25,13 +25,14 @@ func (r *Rank) rank_api() string {
 }
 func (r *Rank) GET_SFACG_RANKS() Template.Rank {
 	var RankStruct Template.Rank
-	params := map[string]string{"size": "50", "rtype": "view", "ntype": "origin", "expand": "typeName,tags,sysTags,ticket,latestchapter"}
+	params := map[string]string{"size": "50", "ntype": "origin", "expand": "typeName,tags,sysTags,ticket,latestchapter"}
 	if r.RtypeIndex >= 6 {
 		fmt.Println("RtypeIndex must be less than 6")
 		r.RtypeIndex = 0
 	}
 	params["page"] = strconv.Itoa(r.Page)
-	params["rtype"] = []string{"view", "sale", "newhit", "mark", "ticket", "bonus"}[r.RtypeIndex]
+	params["rtype"] = map[int]string{0: "view", 1: "sale", 2: "newhit", 3: "mark", 4: "ticket", 5: "bonus"}[r.RtypeIndex]
+
 	request.Get(r.rank_api()).AddAll(params).NewRequests().Unmarshal(&RankStruct).WriteResultString()
 	return RankStruct
 }
