8f113d63981bf0b3645158739f09ab9e619d504b
VeronicaAlexia <Elaina-Alex@proton.me>
2022-11-30 10:09:11 +0800

    ~

--- request/headers.go
+++ request/headers.go
@@ -1,8 +1,23 @@
 package request
 
 import (
+	"crypto/md5"
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"strconv"
 	"strings"
+	"time"
 )
+
+func WeChatBasic() string {
+	signString := "d3htaW5pYXBw" + strconv.FormatInt(time.Now().UnixNano()/1000000, 10) + "null" + "xw3#a12-x"
+	signMd5 := md5.New()
+	signMd5.Write([]byte(signString))
+	Authorization := fmt.Sprintf("wxmpuser:194c5#b_47Fc75676f:nonce=%v&deviceToken=null&timestamp=%v&sign=%v",
+		"d3htaW5pYXBw", strconv.FormatInt(time.Now().UnixNano()/1000000, 10), strings.ToUpper(hex.EncodeToString(signMd5.Sum(nil))))
+	return base64.StdEncoding.EncodeToString([]byte(Authorization))
+}
 
 func (is *HttpUtils) set_headers() {
 	Header := make(map[string]string)
@@ -19,6 +34,7 @@ func (is *HttpUtils) set_headers() {
 		Header["authorization"] = "Basic YW5kcm9pZHVzZXI6MWEjJDUxLXl0Njk7KkFjdkBxeHE="
 	} else {
 		Header["sf-minip-info"] = "minip_novel/1.0.70(android;11)/wxmp"
+		Header["authorization"] = "Basic " + WeChatBasic()
 	}
 	//fmt.Println(Header)
 	for HeaderKey, HeaderValue := range Header {
