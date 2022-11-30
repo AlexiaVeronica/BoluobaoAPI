04261fb3c3820732420cbc37611a89949bcc5bc8
VeronicaAlexia <Elaina-Alex@proton.me>
2022-11-30 19:56:42 +0800

    ~

--- boluobao/tag/tag.go
+++ boluobao/tag/tag.go
@@ -3,13 +3,21 @@ package tag
 import (
 	"fmt"
 	"github.com/VeronicaAlexia/BoluobaoAPI/Template"
+	"github.com/liushuochen/gotable"
+	"strconv"
 	"sync"
 )
 
 func PrintTagList() {
-	for _, value := range GET_SYS_TAG_LIST().Data {
-		fmt.Println("tag_id:", value.SysTagID, "\t\ttag_name:", value.TagName)
+	table, err := gotable.Create("tag_name", "tag_id")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
 	}
+	for _, value := range GET_SYS_TAG_LIST().Data {
+		table.AddRow([]string{value.TagName, strconv.Itoa(value.SysTagID)})
+	}
+	fmt.Println(table)
 }
 func GET_TAG_INFO_ALL(TagID string, last_page int) []Template.Tag {
 	var TagsList []Template.Tag
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/VeronicaAlexia/BoluobaoAPI
 
 go 1.19
 
-require github.com/google/uuid v1.3.0
+require (
+	github.com/google/uuid v1.3.0
+	github.com/liushuochen/gotable v0.0.0-20221119160816-1113793e7092
+)
