5e9d5821cc0aebb35632cbf5c4097166515f7b0f
VeronicaAlexia <Elaina-Alex@proton.me>
2022-11-30 10:26:05 +0800

    ~

--- request/headers.go
+++ request/headers.go
@@ -28,15 +28,16 @@ func (is *HttpUtils) set_headers() {
 		Header["Content-Type"] = "application/json"
 	}
 	Header["Cookie"] = is.Cookie
-	if strings.Contains(is.url, "api.sfacg.com") {
+	if strings.Contains(is.url, "minipapi.sfacg.com") {
+		Header["sf-minip-info"] = "minip_novel/1.0.70(android;11)/wxmp"
+		Header["Authorization"] = "Basic " + WeChatBasic()
+	} else {
 		Header["Host"] = "api.sfacg.com"
 		Header["User-Agent"] = "boluobao/4.8.42(android;25)/XIAOMI/240a90cc-4c40-32c7-b44e-d4cf9e670605/XIAOMI"
-		Header["authorization"] = "Basic YW5kcm9pZHVzZXI6MWEjJDUxLXl0Njk7KkFjdkBxeHE="
-	} else {
-		Header["sf-minip-info"] = "minip_novel/1.0.70(android;11)/wxmp"
-		Header["authorization"] = "Basic " + WeChatBasic()
+		Header["Authorization"] = "Basic YW5kcm9pZHVzZXI6MWEjJDUxLXl0Njk7KkFjdkBxeHE="
+
 	}
-	//fmt.Println(Header)
+	fmt.Println(Header)
 	for HeaderKey, HeaderValue := range Header {
 		is.response.Header.Set(HeaderKey, HeaderValue)
 
