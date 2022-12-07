88608a9ebec41745dff2944e0c2f10d272f8134a
VeronicaAlexia <Elaina-Alex@proton.me>
2022-12-07 22:59:44 +0800

    ~

--- rank_test.go
+++ rank_test.go
@@ -1,19 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"github.com/VeronicaAlexia/BoluobaoAPI/boluobao/rank"
+	"reflect"
 	"testing"
 )
 
+func RangeStruct() []string {
+	// 遍历rank.TypeName
+	var typeName []string
+	v := reflect.ValueOf(rank.TypeName)
+	count := v.NumField()
+	for i := 0; i < count; i++ {
+		f := v.Field(i)
+		switch f.Kind() {
+		case reflect.String:
+			typeName = append(typeName, f.String())
+		case reflect.Int:
+			fmt.Println(f.Int())
+		}
+	}
+	return typeName
+}
 func TestRanks(t *testing.T) {
 	index := 0
-	ranks := rank.Rank{TypeName: rank.TypeName.NewRank, All: true, Page: 0}
-	for _, i2 := range ranks.GET_SFACG_RANKS().Data {
-		index++
-		println(index, i2.NovelName)
-		var a []string
-		for _, tag := range i2.Expand.SysTags {
-			a = append(a, tag.TagName)
+	for _, value := range RangeStruct() {
+		ranks := rank.Rank{TypeName: value, Month: true, Page: 0}
+		for _, i2 := range ranks.GET_SFACG_RANKS().Data {
+			index++
+			println(index, i2.NovelName)
+			var a []string
+			for _, tag := range i2.Expand.SysTags {
+				a = append(a, tag.TagName)
+			}
 		}
 	}
 
