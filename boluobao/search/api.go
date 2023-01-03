715701ee385c43705d4e1f4717e542bf868fd2a0
VeronicaAlexia <Elaina-Alex@proton.me>
2023-01-04 00:00:02 +0800

    ~

---
+++ boluobao/search/api.go
@@ -0,0 +1,20 @@
+package search
+
+import (
+	"fmt"
+	"github.com/VeronicaAlexia/BoluobaoAPI/Template"
+	"github.com/VeronicaAlexia/BoluobaoAPI/request"
+	"strconv"
+)
+
+func GET_SEARCH(keyword string, page int) *Template.Search {
+	var Search Template.Search
+	params := map[string]string{"q": keyword, "page": strconv.Itoa(page), "size": "50"}
+	request.Get("search/novels/result").AddAll(params).NewRequests().Unmarshal(&Search)
+	if Search.Status.HTTPCode == 200 && Search.Data.Novels != nil {
+		return &Search
+	} else {
+		fmt.Println("search failed:", Search.Status.Msg)
+	}
+	return nil
+}
--- boluobao/search/search.go
+++ boluobao/search/search.go
@@ -1,26 +1,11 @@
 package search
 
 import (
-	"fmt"
 	"github.com/VeronicaAlexia/BoluobaoAPI/Template"
 	"github.com/VeronicaAlexia/BoluobaoAPI/pkg/threading"
-	"github.com/VeronicaAlexia/BoluobaoAPI/request"
-	"strconv"
 )
 
-func GET_SEARCH(keyword string, page int) *Template.Search {
-	var Search Template.Search
-	params := map[string]string{"q": keyword, "page": strconv.Itoa(page), "size": "50"}
-	request.Get("search/novels/result").AddAll(params).NewRequests().Unmarshal(&Search)
-	if Search.Status.HTTPCode == 200 && Search.Data.Novels != nil {
-		return &Search
-	} else {
-		fmt.Println("search failed:", Search.Status.Msg)
-	}
-	return nil
-}
-
-func GET_SEARCH_All(keyword string, lastPage int) []Template.BookInfoData {
+func NovelSearch(keyword string, lastPage int) []Template.BookInfoData {
 	var BookInfoList []Template.BookInfoData
 	Thread := threading.InitThreading(64)
 	for i := 0; i < lastPage; i++ {
