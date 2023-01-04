b896952e4b789cb244343e44c961e2ac1bffa702
VeronicaAlexia <Elaina-Alex@proton.me>
2023-01-04 20:53:11 +0800

    ~

--- tag_test.go
+++ tag_test.go
@@ -1,10 +1,16 @@
 package main
 
-//func TestTag(t *testing.T) {
-//	//tag.PrintTagList()
-//	for i, i2 := range booktag.GET_TAG_INFO_ALL("48", 100) {
-//		fmt.Println(i, i2.NovelName)
-//		//fmt.Println("index:", i, "\tbookID:", i2.NovelID, "\tbookName:", i2.NovelName)
-//	}
-//
-//}
+import (
+	"fmt"
+	"github.com/VeronicaAlexia/BoluobaoAPI/boluobao"
+	"testing"
+)
+
+func TestTag(t *testing.T) {
+	//tag.PrintTagList()
+	for i, i2 := range boluobao.API.Tags.NovelTags("48", 100) {
+		//fmt.Println(i, i2.NovelName)
+		fmt.Println("index:", i, "\tbookID:", i2.NovelID, "\tbookName:", i2.NovelName)
+	}
+
+}
